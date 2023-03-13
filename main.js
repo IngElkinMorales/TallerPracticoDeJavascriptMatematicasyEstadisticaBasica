a22d8a8babad0b5867ae46f0f881844602d586e8
IngElkinMorales <stampartcorp@gmail.com>
2023-03-12 19:30:07 -0500

    Se traducen más fórmulas a lenguaje Javascript y se agregan algunos estilos CSS

--- main.js
+++ main.js
@@ -16,10 +16,11 @@ let areaTriangulo = (ladoTrianguloBase*alturaTriangulo)/2;
 
 function calcularTriangulo(lado1,lado2,base){
     let semiperimetro = (lado1 + lado2 + base)*0.5
+   
     return{
         perimetro: lado1+lado2+base + " U",
         area: Math.pow(semiperimetro*(semiperimetro-lado1)*(semiperimetro-lado2)*(semiperimetro-base),0.5) +" U²",
-        alturaLado1: Math.pow((Math.pow(lado1,)))
+        altura: (2*(Math.pow(semiperimetro*(semiperimetro-lado1)*(semiperimetro-lado2)*(semiperimetro-base),0.5)))/base + " U"
     };
 }
 
@@ -36,6 +37,14 @@ function calcularCirculo(radio){
         area: Math.PI*Math.pow(radio,2) + " U²",
     }
 }
+
+console.group();
+
+const radioCirculo = 6;
+const diametroCirculo = radioCirculo*2;
+
+
+console.groupEnd();
 
 console.log({
     ladoCuadrado,
